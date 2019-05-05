cc2bd6e11d0cba5f980acff7efed7936045187cd
Siebe Vanden Eynden <siebe.vandeneynden@gmail.com>
2019-05-05 14:43:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,16 +23,8 @@ SerialPort.list(function(err, ports) {
     pm = port['manufacturer'];
     if (typeof pm !== 'undefined' && pm.includes('arduino')) {
         arduinoport = port.comName.toString();
-        var serialPort = require('serialport');
-        sp = new serialPort(arduinoport, {
-        buadRate: 9600
-        })
-        sp.on('open', function() {
-            console.log('done! arduino is now connected at port: ' + arduinoport)
-            comPort = arduinoport;
-            startListeningRfid();
-        })
         done = true;
+        startListeningRfid();
     }
     if (count === allports && done === false) {
         console.log('cant find arduino')
